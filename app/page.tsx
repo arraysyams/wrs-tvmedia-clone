dd8c8d9b34d4df533dc596ed7d51f42aeb103bd5
Array <57035439+arraysyams@users.noreply.github.com>
2024-07-05 10:32:49 +0800

    Add basic error handler

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,18 @@ import getDataGempa from "@/functions/fetcher";
 import { DataGempaTemplate, DataGempaType } from "@/types/dataGempa";
 
 export default async function Home() {
+  let errTxt: string = ""
   let dataGempa: DataGempaType = DataGempaTemplate;
   try {
     const fetcher = await getDataGempa();
     if (fetcher) {
       dataGempa = fetcher;
     }
-  } catch (error) { }
+  } catch (error:any) { errTxt = error?.message || "Terjadi kesalahan" }
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <InfoGempa initialDataGempa={dataGempa} />
+        { errTxt ? <span>{errTxt}</span> : <InfoGempa initialDataGempa={dataGempa} />}
       </div>
     </main>
   );
